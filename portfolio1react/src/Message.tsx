984604a9fdb5fc01a9666a837bd95972eb1713e2
Rishit Butola <rishitbutola@Rishits-MacBook-Air.local>
2025-02-04 01:28:46 +0530

    understanding

---
+++ portfolio1react/src/Message.tsx
@@ -0,0 +1,9 @@
+function Message(){
+    //JSX :JS XML
+    const name = 'Rishit';
+    if(name)
+      return <h1>Hello {name} </h1>;
+    return <h1>Hellow world!</h1>
+    
+}
+export default Message;
